42530809814f94fc3a5ce4bf46aa5900a35a728d
tmckenna19 <tdlmckenna@gmail.com>
2023-11-16 12:32:53 -0500

    active player animation complete

--- pigGame/script.js
+++ pigGame/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.getElementById('current--0');
@@ -28,7 +30,12 @@ btnRoll.addEventListener('click', function () {
         currentScore += dice; 
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     }else {
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
         activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+
     }
 
     // 2 display dice 
